6d967bfd8fe6e6c600a614528cb34b997cc3dc58
STR02119 <thirajet_p@cmu.ac.th>
2017-06-19 13:20:08 +0700

    mapping

---
+++ src/main/java/com/miniproject/MainSpringBoot.java
@@ -0,0 +1,13 @@
+package com.miniproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * Created by STR02119 on 6/19/2017.
+ */
+@SpringBootApplication
+public class MainSpringBoot {
+    public static void main(String[] args) {
+        SpringApplication.run(MainSpringBoot.class, args);
+    }
+}
---
+++ src/main/java/com/miniproject/com/miniproject/controller/Mapping.java
@@ -0,0 +1,28 @@
+package com.miniproject.com.miniproject.controller;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by STR02119 on 6/19/2017.
+ */
+@Controller
+public class Mapping {
+
+    @Value("${home.title:test}")
+    private String title = "";
+
+    @RequestMapping("/")
+    public String homevoid(Map<String, Object> model) {
+        model.put("title", this.title);
+        return "home";
+    }
+
+    @RequestMapping("/home")
+    public String Home (){
+        return "home";
+    }
+}
